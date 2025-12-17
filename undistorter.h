d6e9e25a97b0327dbcafb2763f82388f64b61c6c
Nathan <nathanmolson@gmail.com>
2025-12-16 16:50:37 -0800

    missed last commit

--- undistorter.h
+++ undistorter.h
@@ -12,11 +12,13 @@ public:
     std::vector<cv::Point2f> GetPixelCoords(const std::vector<cv::Point3f> &rays_cam) const;
     cv::Point2f GetPixelCoords(const cv::Point3f &ray_cam) const;
     cv::Mat Undistort(const cv::Mat &img) const;
+    float MaxXYLength() const;
 
 private:
     cv::Mat _cam_old;
     cv::Mat _cam_new;
     std::vector<double> _dist_coeffs;
+    float _max_xy_length;
 
     cv::Mat _map1;
     cv::Mat _map2;
