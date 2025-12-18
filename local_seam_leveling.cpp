2f9f9185d491e9c7d0487057d1ca16c8033dde70
Nathan <nathanmolson@gmail.com>
2025-12-18 14:44:08 -0800

    slight speedup and vast memory reduction by storing local leveling coefficients as sparse

--- local_seam_leveling.cpp
+++ local_seam_leveling.cpp
@@ -1,41 +1,44 @@
 #include "local_seam_leveling.h"
 
+#include <iostream>
+#include <chrono>
+#include <map>
+
 cv::Mat local_seam_leveling(const cv::Mat &labels, const cv::Mat mosaic)
 {
+    auto t1 = std::chrono::steady_clock::now();
     int tile_width = mosaic.rows / labels.rows;
-    constexpr int num_coeffs = 6;
-    std::vector<float> coeffs(mosaic.rows * mosaic.cols * num_coeffs, 1.F);
-
-    for (size_t k = 0; k < coeffs.size(); k += num_coeffs)
-    {
-        coeffs[k] = 4.F;
-        coeffs[k + 5] = 0.F;
-    }
+    std::map<int, std::array<float, 6>> coeffs;
+    std::array<float, 6> baseline = {4, 1, 1, 1, 1, 0};
 
     for (int i = 0; i < mosaic.rows; i++)
     {
         // Left
-        int k = num_coeffs * (i * mosaic.cols);
-        coeffs[k] -= 1;
-        coeffs[k + 4] = 0;
+        int k = i * mosaic.cols;
+        coeffs.try_emplace(k, baseline);
+        coeffs[k][0] -= 1;
+        coeffs[k][4] = 0;
 
         // Right
-        k = num_coeffs * (i * mosaic.cols + mosaic.cols - 1);
-        coeffs[k] -= 1;
-        coeffs[k + 2] = 0;
+        k = i * mosaic.cols + mosaic.cols - 1;
+        coeffs.try_emplace(k, baseline);
+        coeffs[k][0] -= 1;
+        coeffs[k][2] = 0;
     }
 
     for (int j = 0; j < mosaic.cols; j++)
     {
         // Top
-        int k = num_coeffs * j;
-        coeffs[k] -= 1;
-        coeffs[k + 1] = 0;
+        int k = j;
+        coeffs.try_emplace(k, baseline);
+        coeffs[k][0] -= 1;
+        coeffs[k][1] = 0;
 
         // Bottom
-        k = num_coeffs * ((mosaic.rows - 1) * mosaic.cols + j);
-        coeffs[k] -= 1;
-        coeffs[k + 3] = 0;
+        k = (mosaic.rows - 1) * mosaic.cols + j;
+        coeffs.try_emplace(k, baseline);
+        coeffs[k][0] -= 1;
+        coeffs[k][3] = 0;
     }
 
     for (int label_i = 0; label_i < labels.rows; label_i++)
@@ -48,14 +51,16 @@ cv::Mat local_seam_leveling(const cv::Mat &labels, const cv::Mat mosaic)
                 int j = label_j * tile_width;
                 for (int i = label_i * tile_width; i < (label_i + 1) * tile_width; i++)
                 {
-                    int kr = num_coeffs * (i * mosaic.cols + j);
-                    int kl = kr - num_coeffs;
+                    int kr = i * mosaic.cols + j;
+                    int kl = kr - 1;
 
-                    coeffs[kr] -= 1;
-                    coeffs[kr + 4] = 0;
+                    coeffs.try_emplace(kr, baseline);
+                    coeffs[kr][0] -= 1;
+                    coeffs[kr][4] = 0;
 
-                    coeffs[kl] -= 1;
-                    coeffs[kl + 2] = 0;
+                    coeffs.try_emplace(kl, baseline);
+                    coeffs[kl][0] -= 1;
+                    coeffs[kl][2] = 0;
                 }
             }
             // Seam
@@ -64,17 +69,19 @@ cv::Mat local_seam_leveling(const cv::Mat &labels, const cv::Mat mosaic)
                 int j = label_j * tile_width;
                 for (int i = label_i * tile_width; i < (label_i + 1) * tile_width; i++)
                 {
-                    int kr = num_coeffs * (i * mosaic.cols + j);
-                    int kl = kr - num_coeffs;
-                    float seam =  ((int)mosaic.at<uint16_t>(i, j) - (int)mosaic.at<uint16_t>(i, j - 1));
+                    int kr = i * mosaic.cols + j;
+                    int kl = kr - 1;
+                    float seam = ((int)mosaic.at<uint16_t>(i, j) - (int)mosaic.at<uint16_t>(i, j - 1));
 
-                    coeffs[kr] += 1;
-                    coeffs[kr + 4] = 0;
-                    coeffs[kr + 5] -= seam;
+                    coeffs.try_emplace(kr, baseline);
+                    coeffs[kr][0] += 1;
+                    coeffs[kr][4] = 0;
+                    coeffs[kr][5] -= seam;
 
-                    coeffs[kl] += 1;
-                    coeffs[kl + 2] = 0;
-                    coeffs[kl + 5] += seam;
+                    coeffs.try_emplace(kl, baseline);
+                    coeffs[kl][0] += 1;
+                    coeffs[kl][2] = 0;
+                    coeffs[kl][5] += seam;
                 }
             }
         }
@@ -90,14 +97,16 @@ cv::Mat local_seam_leveling(const cv::Mat &labels, const cv::Mat mosaic)
                 int i = label_i * tile_width;
                 for (int j = label_j * tile_width; j < (label_j + 1) * tile_width; j++)
                 {
-                    int kb = num_coeffs * (i * mosaic.cols + j);
-                    int kt = kb - num_coeffs * mosaic.cols;
+                    int kb = i * mosaic.cols + j;
+                    int kt = kb - mosaic.cols;
 
-                    coeffs[kb] -= 1;
-                    coeffs[kb + 1] = 0;
+                    coeffs.try_emplace(kb, baseline);
+                    coeffs[kb][0] -= 1;
+                    coeffs[kb][1] = 0;
 
-                    coeffs[kt] -= 1;
-                    coeffs[kt + 3] = 0;
+                    coeffs.try_emplace(kt, baseline);
+                    coeffs[kt][0] -= 1;
+                    coeffs[kt][3] = 0;
                 }
             }
             // Seam
@@ -106,17 +115,19 @@ cv::Mat local_seam_leveling(const cv::Mat &labels, const cv::Mat mosaic)
                 int i = label_i * tile_width;
                 for (int j = label_j * tile_width; j < (label_j + 1) * tile_width; j++)
                 {
-                    int kb = num_coeffs * (i * mosaic.cols + j);
-                    int kt = kb - num_coeffs * mosaic.cols;
-                    float seam =  ((int)mosaic.at<uint16_t>(i, j) - (int)mosaic.at<uint16_t>(i - 1, j));
+                    int kb = i * mosaic.cols + j;
+                    int kt = kb - mosaic.cols;
+                    float seam = ((int)mosaic.at<uint16_t>(i, j) - (int)mosaic.at<uint16_t>(i - 1, j));
 
-                    coeffs[kb] += 1;
-                    coeffs[kb + 1] = 0;
-                    coeffs[kb + 5] -= seam;
+                    coeffs.try_emplace(kb, baseline);
+                    coeffs[kb][0] += 1;
+                    coeffs[kb][1] = 0;
+                    coeffs[kb][5] -= seam;
 
-                    coeffs[kt] += 1;
-                    coeffs[kt + 3] = 0;
-                    coeffs[kt + 5] += seam;
+                    coeffs.try_emplace(kt, baseline);
+                    coeffs[kt][0] += 1;
+                    coeffs[kt][3] = 0;
+                    coeffs[kt][5] += seam;
                 }
             }
         }
@@ -131,17 +142,29 @@ cv::Mat local_seam_leveling(const cv::Mat &labels, const cv::Mat mosaic)
         {
             for (int j = 1; j < adjustments.cols - 1; j++)
             {
-                int k = num_coeffs * (i * mosaic.cols + j);
-                tmp.at<float>(i, j) = (coeffs[k + 1] * adjustments.at<float>(i - 1, j) +
-                                       coeffs[k + 2] * adjustments.at<float>(i, j + 1) +
-                                       coeffs[k + 3] * adjustments.at<float>(i + 1, j) +
-                                       coeffs[k + 4] * adjustments.at<float>(i, j - 1) +
-                                       coeffs[k + 5]) /
-                                      coeffs[k];
+                tmp.at<float>(i, j) = (adjustments.at<float>(i - 1, j) +
+                                       adjustments.at<float>(i, j + 1) +
+                                       adjustments.at<float>(i + 1, j) +
+                                       adjustments.at<float>(i, j - 1)) /
+                                      4.F;
             }
+        }
+        for (auto [k, c] : coeffs)
+        {
+            int i = k / adjustments.cols;
+            int j = k % adjustments.cols;
+            tmp.at<float>(i, j) = (c[1] * adjustments.at<float>((i + adjustments.rows - 1) % adjustments.rows, j) +
+                                   c[2] * adjustments.at<float>(i, (j + 1) % adjustments.cols) +
+                                   c[3] * adjustments.at<float>((i + 1) % adjustments.rows, j) +
+                                   c[4] * adjustments.at<float>(i, (j + adjustments.cols - 1) % adjustments.cols) +
+                                   c[5]) /
+                                  c[0];
         }
         adjustments = tmp;
     }
+    auto t2 = std::chrono::steady_clock::now();
+
+    std::cout << "Local leveling time: " << std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1).count() << std::endl;
 
     return adjustments;
 }
